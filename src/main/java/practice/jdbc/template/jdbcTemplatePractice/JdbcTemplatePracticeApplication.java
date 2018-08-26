d987d6960bdf33faa0ce0e4555106773cdf2f354
skuarch <skuarch@yahoo.com.mx>
2018-08-26 15:35:22 -0500

    New repository

---
+++ src/main/java/practice/jdbc/template/jdbcTemplatePractice/JdbcTemplatePracticeApplication.java
@@ -0,0 +1,12 @@
+package practice.jdbc.template.jdbcTemplatePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplatePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplatePracticeApplication.class, args);
+	}
+}
